77b754e40b89c45916331d00cad97739180648a7
svis <sergey.visker@gmail.com>
2020-06-23 19:55:42 +0300

    Initial commit

---
+++ src/main/java/com/svis/voting_rest/VotingRestApplication.java
@@ -0,0 +1,13 @@
+package com.svis.voting_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotingRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VotingRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/svis/voting_rest/VotingRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.svis.voting_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotingRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
